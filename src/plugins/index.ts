5d229fdaafa398b10819f071a2f6440de37b8cf1
SaberWang8023 <saberwang8023@163.com>
2022-11-09 18:57:25 +0800

    perf: 项目优化插件注册的TS提示问题

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -2,7 +2,5 @@ import { type VuePluginOptions } from 'types'
 import { type App, type Plugin } from 'vue'
 
 export const registerPlugin = (app: App<Element>, options: VuePluginOptions) => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  Object.values(import.meta.glob<{ install: Plugin }>(['./*.ts', '!./index.ts'], { eager: true })).forEach((i) => app.use(i, options))
+  Object.values(import.meta.glob<Plugin>(['./*.ts', '!./index.ts'], { eager: true })).forEach((i) => app.use(i, options))
 }
